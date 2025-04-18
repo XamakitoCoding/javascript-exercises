41889dc6a1073a585b534d9014f379f0b52daa31
XamakitoCoding <114276144+XamakitoCoding@users.noreply.github.com>
2025-04-17 21:56:28 -0400

    Started the repeatString.js excercise | Passed the first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    const stringArray = [];
 
+    for (i = 0; i < num; i++) {
+        stringArray.push(string);
+    }
+
+    return stringArray.join('');
 };
 
 // Do not edit below this line
