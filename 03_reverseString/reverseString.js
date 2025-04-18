1e04b508f03569e7bc046e0bc722b4a6a9af5056
XamakitoCoding <114276144+XamakitoCoding@users.noreply.github.com>
2025-04-18 19:29:04 -0400

    Finished this excercise | I'm not sure if i want to commit this but i will use it as a remainder to myself to never do this again

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let inputLength = input.length;
+    let reversedInput = '';
 
+    for (i = (inputLength - 1); i >= 0; i--) {
+        reversedInput += input.charAt(i);
+    }
+    return reversedInput;
 };
 
 // Do not edit below this line
