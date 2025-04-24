bb51375d04a7dc46bebf67bf7a48640520281bc9
XamakitoCoding <114276144+XamakitoCoding@users.noreply.github.com>
2025-04-23 23:27:17 -0400

    Made the fifth excercise || Easier than expected, but my solution may not be so clean

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
 
+    let sum = 0;
+
+    if (int1 > -1 && int2 > -1 && Number.isInteger(int1) == true && Number.isInteger(int2) == true) {
+        if (int1 < int2) {
+            for (let i = int1; i <= int2; i++) {
+                sum += i; 
+            }
+        } else {
+            for (let i = int2; i <= int1; i++) {
+                sum += i; 
+            }
+        }
+    return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
